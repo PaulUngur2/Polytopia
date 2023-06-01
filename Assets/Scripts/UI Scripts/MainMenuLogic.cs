40083a341d0b8914b29111dffcd148c65f17bc3d
Mihai Tanasa <talexmihai@yahoo.com>
2023-06-01 22:40:23 +0300

    Changed main menu

--- Assets/Scripts/UI Scripts/MainMenuLogic.cs
+++ Assets/Scripts/UI Scripts/MainMenuLogic.cs
@@ -1,24 +1,75 @@
+using System;
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 using UnityEngine.SceneManagement;
 using UnityEngine.UIElements;
 
 public class MainMenuLogic : MonoBehaviour
 {
-    private void OnEnable() {
-        VisualElement root = GetComponent<UIDocument>().rootVisualElement;
+    
+    [Range(0,1)]
+    public float turnAngle = 0.2f;
+    public Volume postProcessingVolume;
+    public GameObject otherUIs;
+    private GameObject thisUI;
+    private Camera mainCamera;
+    private Camera menuCamera;
+    private DepthOfField dof;
+    private float initialFocusDistance;
 
+    private void OnEnable()
+    {
+        // Disable the other UIs and find the current one
+        otherUIs.SetActive(false);
+        thisUI = GameObject.Find("MainMenuUI");
+
+        // Find the two cameras and assign them
+        Camera[] cameras = Camera.allCameras;
+        if (cameras[0].name.Equals("Main Camera"))
+        {
+            mainCamera = cameras[0];
+            menuCamera = cameras[1];
+        }
+        else
+        {
+            mainCamera = cameras[1];
+            menuCamera = cameras[0];
+        }
+        mainCamera.enabled = false;
+        menuCamera.enabled = true;
+
+        // TODO Find the volume instead of assigning it in the editor
+        // and modify it to add blur 
+        postProcessingVolume.profile.TryGet(out dof);
+        initialFocusDistance = dof.focusDistance.value;
+        dof.focusDistance.value = 5;
+        
+        // Find the UI elements and initialize them
+        VisualElement root = GetComponent<UIDocument>().rootVisualElement;
         Button playButton = root.Q<Button>("PlayButton");
         Button exitButton = root.Q<Button>("ExitButton");
-     
         playButton.clicked += StartGame;
         exitButton.clicked += ExitGame;
     }
-    
-    private void StartGame() {
-        SceneManager.LoadScene("GameScene");
+
+    private void FixedUpdate()
+    {
+        menuCamera.transform.RotateAround(new Vector3(20, 0, 0), Vector3.up, turnAngle);
     }
-    
-    private void ExitGame() {
+
+    private void StartGame()
+    {
+        // SceneManager.LoadScene("GameScene");
+        menuCamera.enabled = false;
+        mainCamera.enabled = true;
+        thisUI.SetActive(false);
+        otherUIs.SetActive(true);
+        dof.focusDistance.value = initialFocusDistance;
+    }
+
+    private void ExitGame()
+    {
         Application.Quit(0);
     }
 }
